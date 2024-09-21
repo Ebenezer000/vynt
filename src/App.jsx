25fd2e63866ada96e19e3852cc74493e40b2857d
Ebenezer Akpas <akpasebenezer0@gmail.com>
2024-09-21 02:57:18 +0100

    use small letter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Footer from "./layout/footer/main/main"
 import Navbar from "./layout/header/nav/main"
-import Faq from "./layout/Landing/Faq/main"
+import Faq from "./layout/landing/Faq/main"
 import Hero from "./layout/landing/home/hero"
-import JoinUs from "./layout/Landing/joinus/main"
-import Testimonials from "./layout/Landing/testimonials/main"
+import JoinUs from "./layout/landing/joinus/main"
+import Testimonials from "./layout/landing/testimonials/main"
 
 
 function App() {
